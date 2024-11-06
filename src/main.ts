3cb49ad875964699753774f335bd4487c749f392
monfaredman <monfaredman@outlook.com>
2024-11-06 15:28:35 +0330

    config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
+import { SwaggerConfigInit } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  SwaggerConfigInit(app);
+  const { PORT } = process.env;
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Swagger is running on http://localhost:${PORT}/swagger`);
+  });
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { TypeOrmConfig } from 'src/config/typeorm.config';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig())],
   controllers: [AppController],
   providers: [AppService],
 })
